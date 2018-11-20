0020d6a0a7e3e44b4e48ff3f3dd2265f0f0dd21f
spiderM96 <343627441@qq.com>
2018-11-20 14:14:21 +0800

    commit cookie

--- src/main/java/com/mmall/utils/CookieUtil.java
+++ src/main/java/com/mmall/utils/CookieUtil.java
@@ -1,8 +1,10 @@
 package com.mmall.utils;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -13,16 +15,46 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class CookieUtil {
 
-    private final static String COOKIE_DOMAIN = "";
-    private final static String COOKIE_NAME = "";
+    private final static String COOKIE_DOMAIN = ".happymmall.com";
+    private final static String COOKIE_NAME = "mmall_login_token";
 
     public static void writeLoginToken(HttpServletResponse response,String token){
         Cookie cookie = new Cookie(COOKIE_NAME,token);
         cookie.setDomain(COOKIE_DOMAIN);
         cookie.setPath("/");
 
-        cookie.setMaxAge(60*60*24*365);
+        cookie.setMaxAge(60*60*24*365);//-1是永久
+        log.info("write cookieName:{},cookieValue:{}",cookie.getName(),cookie.getValue());
         response.addCookie(cookie);
 
+    }
+
+    public static String readLoginToken(HttpServletRequest httpServletRequest){
+        Cookie[] cookies = httpServletRequest.getCookies();
+        if(cookies!=null){
+            for(Cookie cookie : cookies){
+                log.info("read cookieName:{},cookieValue:{}",cookie.getName(),cookie.getValue());
+                if (StringUtils.equals(COOKIE_NAME,cookie.getName())){
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void deleteLoginToken(HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse){
+        Cookie[] cookies = httpServletRequest.getCookies();
+        if(cookies!=null){
+            for(Cookie cookie : cookies){
+                log.info("read cookieName:{},cookieValue:{}",cookie.getName(),cookie.getValue());
+                if (StringUtils.equals(COOKIE_NAME,cookie.getName())){
+                    cookie.setDomain(COOKIE_DOMAIN);
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);//0，代表删除此cookie
+                    httpServletResponse.addCookie(cookie);
+                    return;
+                }
+            }
+        }
     }
 }
